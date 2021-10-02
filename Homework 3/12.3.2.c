1d98b5cd99694dc89a46083e947724314759a664
YEonleo <90837906+YEonleo@users.noreply.github.com>
2021-10-02 15:35:56 +0900

    Create 12.3.2.c

---
+++ Homework 3/12.3.2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct date{
+    int year;
+    int month;
+    int day;
+};
+
+int days(struct date*,int*);
+
+int main(){
+
+    struct date day;
+    //save data in structure
+    while(1){
+        int res;
+        printf("Type date (Month day year): ");
+        scanf("%d %d %d",&day.month,&day.day,&day.year);
+
+        days(&day,&res);
+
+        printf("Days: %d\n",res);
+    }
+    return 0;   
+}
+
+int days(struct date *days,int *res){
+    //change value by its address that reduce from what i saved in structure days
+    *res = (days->day-1)+(days->month-1)*30+(days->year-2000)*360;
+}
